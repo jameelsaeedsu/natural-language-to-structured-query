a4e3ee38ae29e0b3c2649b6a622ef4a456403ca0
jameelsaeedsu <jameel.saeed@hotmail.com>
2025-01-09 19:32:29 +0100

    first commit

---
+++ src/main/java/com/jamme/dev/nlp/to/query/natural_language_to_structure_query/NaturalLanguageToStructureQueryApplication.java
@@ -0,0 +1,13 @@
+package com.jamme.dev.nlp.to.query.natural_language_to_structure_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalLanguageToStructureQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalLanguageToStructureQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamme/dev/nlp/to/query/natural_language_to_structure_query/NaturalLanguageToStructureQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamme.dev.nlp.to.query.natural_language_to_structure_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalLanguageToStructureQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
